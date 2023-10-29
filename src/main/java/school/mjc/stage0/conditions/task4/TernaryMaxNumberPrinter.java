efa63aa746b5fd7f3cc54d7cb53f4371dcfe35d8
yoqub-dev <yoqub.epam@gmail.com>
2023-10-30 03:04:25 +0500

    task9

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatest = (first > second) ? ((first > third) ? first : third) : ((second > third) ? second : third);
+        System.out.println(greatest);
     }
 }
